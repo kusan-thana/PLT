08f08a8083ce14e054866c8485d91fcbdb363f94
benoit lafon <benoit.lafon92@gmail.com>
2015-10-20 17:16:54 +0200

    Render Code

--- src/tileset.hpp
+++ src/tileset.hpp
@@ -2,14 +2,16 @@
 #define TileSet_H
 #include <vector>
 #include "statictile.hpp"
+#include "element.hpp"
+#include "tile.hpp"
 
 class TileSet {
-	
+
 	protected:
-		std::vector<StaticTile>staticTiles;
+		std::vector<StaticTile> staticTiles;
 
 	public:
-
+		const Tile* const getElementTile(const Element* element);
 };
 
 #endif
