561d0a2c69c84045a9ab8d7c17602bfa58190619
Benoît Lafon <benoit.lafon92@gmail.com>
2015-11-30 21:59:50 +0100

    heuristicAI

--- src/heuristicAi.cpp
+++ src/heuristicAi.cpp
@@ -13,28 +13,27 @@ HeuristicAI::HeuristicAI(state::LevelState& mainLevelState) : DumbAI(mainLevelSt
 }
 bool HeuristicAI::moveToClosest(engine::Engine& engine,const PathMap& path, state::Element* element){
 	
-	std::cout << "moveToClosest\n";
-	int width = path.getWidth();
-	
 	int x_elem = element->getX();
 	int y_elem = element->getY();
+	int nbStep = ((state::MobileElement*)element)->getNbStep();
 	
 	int x_move = 0;
 	int y_move = 0;
 	
+	int width = path.getWidth();
 	int* weights = path.getWeights();
 	
 	int value = 999;
 	
-	for(int i=-1; i<=1; i++){
-		for(int j=-1; j<=1; j++){
+	for(int i=0; i<2*nbStep+1; i++){
+		for(int j=0; j<2*nbStep+1; j++){
 			
-			int x_tmp = x_elem+i;
-			int y_tmp = y_elem+j;
+			int x_tmp = x_elem - nbStep + i;
+			int y_tmp = y_elem - nbStep + j;
 			
 			if(path.isValid(x_tmp, y_tmp) && weights[x_tmp * width + y_tmp]<999){		//on s'assure que la case est dans las grille et n'est pas un obstacle ou un personnage
 				
-				if(weights[x_tmp * width + y_tmp] <  value){
+				if(weights[x_tmp * width + y_tmp] <  value && weights[x_tmp * width + y_tmp] !=0){		//on cherche la case avec le poids le moins fort dans la limite du nombre de pas
 					value = weights[x_tmp * width + y_tmp];
 					x_move = x_tmp;
 					y_move = y_tmp;
@@ -42,11 +41,13 @@ bool HeuristicAI::moveToClosest(engine::Engine& engine,const PathMap& path, stat
 			}
 		}
 	}
-	if(value<999){		//si un déplacement est réalisable, on met à jour les coordonnées de l'élément
-		
-		std::cout << "value inf 999\n";
-		element->setX(x_move);
-		element->setY(y_move);
+	if(value<999){		//si un déplacement est réalisable, on créée une commande de déplacement
+		std::cout << "x_elem " << x_elem << std::endl;
+		std::cout << "y_elem " << y_elem << std::endl;
+		std::cout << "x " << x_move << std::endl;
+		std::cout << "y " << y_move << std::endl;
+		engine::MoveCommand* move = new engine::MoveCommand(x_move, y_move, element);
+		engine.addCommand(move);
 		return true;
 	}
 	return false;
@@ -58,17 +59,6 @@ void HeuristicAI::run(engine::Engine& engine){
 	
 	playerCharsMap.clear();																//on réinitialise les éléments de la carte de distance
 	
-	for(int i=0; i<characters.size(); i++){												//on calcul la carte de distance des personnages du joueurs
-		
-		if(((state::MobileElement*)characters.getElement(i))->isPlayerCharacter()){		//on récupère un personnage du joueur 
-			
-			std::cout << "addElement\n";
-			playerCharsMap.addElement(characters.getElement(i));						//on l'ajoute sur la carte des distances
-		}
-	}
-	playerCharsMap.dijsktra();
-	playerCharsMap.display();
-	
 	for(int i=0; i<characters.size(); i++){												//on détermine le prochain mouvement des monstres
 		
 		state::Element* curr_element = characters.getElement(i);
@@ -76,16 +66,18 @@ void HeuristicAI::run(engine::Engine& engine){
 		if( ! ((state::MobileElement*)curr_element)->isPlayerCharacter()){				//on récupère un monstre 
 			if (((state::MobileElement*)curr_element)->getTurnPlayed() == false){		//on s'assure qu'il peut jouer
 				
-				if(moveToClosest(engine, playerCharsMap, curr_element)){
-				
-					int x = curr_element->getX();
-					int y = curr_element->getY();
-					std::cout << "x " << x << std::endl;
-					std::cout << "y " << y << std::endl;
-				
-					engine::MoveCommand* move = new engine::MoveCommand(x, y, curr_element);
-					engine.addCommand(move);
+				for(int j=0; j<characters.size(); j++){												//on calcul la carte de distance des personnages du joueurs
+		
+					if(((state::MobileElement*)characters.getElement(j))->isPlayerCharacter()){		//on récupère un personnage du joueur 
+			
+						playerCharsMap.addElement(characters.getElement(j));						//on l'ajoute sur la carte des distances
+					}
 				}
+				playerCharsMap.dijsktra();
+				
+				moveToClosest(engine, playerCharsMap, curr_element);
+				
+				playerCharsMap.display();
 			}
 		}
 	}
--- src/pathMap.cpp
+++ src/pathMap.cpp
@@ -136,12 +136,16 @@ void PathMap::dijsktra(){
 	}
 }
 void PathMap::display(){
-	std::cout << "----------------------------------------------------------------" << std::endl;
-	std::cout << "-----------ENLARGE THE BASH WINDOW BEFORE RUNNING!!-------------" << std::endl;
-	std::cout << "--------------Path Map using Dijkstra algorithm-----------------" << std::endl;
-	std::cout << "------------------Display from SFMLClient-----------------------" << std::endl;
-	std::cout << "----------------------------------------------------------------" << std::endl;
+	std::cout << "------------------------------------------------------------------------------------" << std::endl;
+	std::cout << "------MAY NEED TO ENLARGE THE BASH WINDOW BEFORE RUNNING FOR A BETTER DISPLAY-------" << std::endl;
+	std::cout << "---------------------Path Map using Dijkstra algorithm------------------------------" << std::endl;
+	std::cout << "------------------------------------------------------------------------------------" << std::endl;
 	
+	std::cout <<  "First row -> columns index" << std::endl;
+	for(int j=0; j<width; j++){
+			std::cout.width(4); std::cout << std::left << j;
+		}
+		std::cout << std::endl;
 	for(int i=0; i<height; i++){
 		for(int j=0; j<width; j++){
 			std::cout.width(4); std::cout << std::left << weights[j + i*width];
