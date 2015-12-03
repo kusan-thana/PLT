6e93bf179510a20faf71173a07dcef5c7e394575
kusan-thana <kusan.thana@gmail.com>
2015-12-03 21:41:39 +0100

    manuel de jeu (GUI) corrigé

--- src/gui.cpp
+++ src/gui.cpp
@@ -19,7 +19,7 @@ GUI::~GUI(){
 }
 void GUI::init() {
 
-	std::cout << std::endl << "Manuel de jeu : \nSelection : espace ou clic gauche\nDeselectionner: Echap ou clic droit\nDouble selectionner un personnage pour terminer son tour\n\nMODE AUTO (Commandes Player : dumbIA) : ENTREE" << std::endl;
+	std::cout << std::endl << "Manuel de jeu : \nSelection : espace ou clic gauche\nDeselectionner: Echap ou clic droit\nDouble selectionner un personnage pour terminer son tour\n\nMODE AUTO (Commandes Player : HeuristicAI) : ENTREE" << std::endl;
 	engine::LoadCommand* load = new engine::LoadCommand("../res/level2.txt");
 	//~ engine.addCommand(load); //initialiser ici le 1er niveau
 }
