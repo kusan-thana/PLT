ac5b3dc7ece3db37b2a9a57a5234e9f731cc423c
Kusan Thanabalasingam <kusan.thana@gmail.com>
2015-12-08 14:15:43 +0100

    configure

--- src/generated/render.h
+++ src/generated/render.h
@@ -1,43 +1,17 @@
 #ifndef RENDER__H
 #define RENDER__H
 
-#include "state.h"
-
 namespace render {
 
-  /// class Tile - 
-  class Tile {
-    // Operations
-  public:
-    virtual ~Tile ();
-    virtual bool const isAnimated ();
-  };
-
-  /// class TileSet - Stocke un ensemble de tuiles et animations possibles pour plan donnée
+  /// class TileSet - 
   class TileSet {
-    // Associations
     // Operations
   public:
     virtual ~TileSet ();
     virtual int const getCellWidth () = 0;
-    virtual int const getCellWidth () = 0;
+    virtual int const getCellHeight () = 0;
     virtual const char* const getImageFile () = 0;
-    virtual const Tile* const getElementTile (const Element* element) = 0;
-  };
-
-  /// class Animation - 
-  class Animation {
-    // Associations
-    // Attributes
-  public:
-    int i;
-  protected:
-    int x;
-    int y;
-    const Tile* tile;
-    // Operations
-  public:
-    void Animation (int i, int x, int y, Tile* tile);
+    virtual const render::Tile* const getCursorTile () = 0;
   };
 
   /// class Surface - 
@@ -49,113 +23,7 @@ namespace render {
     virtual void loadTexture (const char* image_file) = 0;
     virtual void setSpriteCount (int n) = 0;
     virtual void setSpriteLocation (int i, int x, int y) = 0;
-    virtual void setSpriteTexture (int i, const StaticTile* staticTile) = 0;
-  };
-
-  /// class Layer - 
-  class Layer {
-    // Associations
-    // Attributes
-  protected:
-    Surface* surface;
-    const TileSet* tileSet;
-    std::map<int,Animation*> animations;
-    // Operations
-  public:
-    Layer ();
-    virtual ~Layer ();
-    const TileSet* const getTileSet ();
-    void setTileSet (const TileSet* tileSet);
-    void setSurface (Surface* surface);
-  };
-
-  enum SceneLayer {
-    GRID_LAYER,
-    CURSORS_LAYER,
-    CHARACTERS_LAYER,
-    STATE_LAYER
-  };
-
-  /// class ElementListLayer - 
-  class ElementListLayer : public render::Layer {
-    // Operations
-  public:
-    void levelStateChanged (const state::LevelStateEvent& e);
-  };
-
-  /// class Scene - 
-  class Scene : public state::LevelStateObserver {
-    // Associations
-    // Attributes
-  public:
-    int width;
-    int height;
-    std::vector<Layer*> layers;
-    // Operations
-  public:
-    Scene ();
-    ~Scene ();
-    void setLayer (int idx, Layer* layer);
-    void levelStateChanged (const state::LevelStateChanged& e);
-  };
-
-  /// class StaticTile - 
-  class StaticTile : public render::Tile {
-    // Attributes
-  protected:
-    int x;
-    int y;
-    int width;
-    int height;
-    // Operations
-  public:
-    StaticTile (int x, int y, int width, int heigth);
-    bool const isAnimated ();
-    int const getX ();
-    int const getY ();
-    int const getWidth ();
-    int const getHeight ();
-    void setX (int x);
-    void setY (int y);
-    void setWidth (int width);
-    void setHeight (int height);
-  };
-
-  /// class AnimatedTile - 
-  class AnimatedTile : public render::Tile {
-    // Associations
-    // Attributes
-  public:
-    std::vector<const Tile*> tiles;
-    float rate;
-    // Operations
-  public:
-    bool const isAnimated ();
-    float const getRate ();
-    const Tile* const getTile (int i);
-    void addTile (Tile* tle);
-    void setRate (float rate);
-  };
-
-  /// class SFMLSurface - 
-  class SFMLSurface : public render::Surface {
-    // Attributes
-  protected:
-    sf::VertexArray vertices;
-    sf::Texture tileSet;
-    int widthSprite;
-    int heigthSprite;
-    // Operations
-  public:
-    void clear ();
-    void loadTexture (const char* tileset);
-    void setSpriteCount (int n);
-    void setSpriteLocation (int i, int x, int y);
-    void setSpriteTexture (int i, const StaticTile* staticTile);
-    void setSpriteWidth (int widthSprite, int heightSprite);
-    SFMLSurface& getSfmlSurface ();
-  private:
-    void const draw (sf::RenderTarget& target, sf::RenderStates states);
+    virtual void setSpriteTexture (int i, const render::StaticTile* staticTile) = 0;
   };
 
 };
--- src/generated/state.h
+++ src/generated/state.h
@@ -1,13 +1,457 @@
 #ifndef STATE__H
 #define STATE__H
 
+
 namespace state {
+
+  enum TypeID {
+    SPACE     = 1,
+    OBSTACLE     = 2,
+    HERO     = 3,
+    BRUTE     = 4,
+    HEALER     = 5,
+    WIZARD     = 6,
+    MINION     = 7,
+    BOSS     = 8
+  };
+
+  enum Direction {
+    NONE     = 0,
+    NORTH     = 1,
+    SOUTH     = 2,
+    EAST     = 3,
+    WEST     = 4
+  };
+
+  /// class Element - 
+  class Element {
+    // Associations
+    state::TypeID IDElement;
+    // Attributes
+  public:
+    int x_origin;
+    int y_origin;
+  protected:
+    int x;
+    int y;
+    Direction orientation;
+    /// "True" quand l'element est survole
+    bool active;
+    // Operations
+  public:
+    /// Constructeur
+    Element ();
+    /// Destructeur
+    virtual ~Element ();
+    virtual bool const isStatic () = 0;
+    virtual Element* const clone () = 0;
+    virtual TypeID const getTypeID ();
+    int const getX ();
+    int const getY ();
+    Direction const getDirection ();
+    void setX (int x);
+    void setY (int y);
+    void setDirection (Direction orientation);
+    bool const isActive ();
+    void setActive (bool active);
+  };
+
+  enum Status {
+    NORMAL,
+    SUPER
+  };
+
+  /// class MobileElement - 
+  class MobileElement : public state::Element {
+    // Associations
+    state::Status status;
+    // Attributes
+  protected:
+    int speed;
+    /// Health Point : Nombre de point de vie
+    int HP;
+    /// Point d'attaque
+    int attackPt;
+    /// Point de défense
+    int defensePt;
+    /// Nombre  d'action par tour
+    int nbAction;
+    /// Nombre de cases de déplacement possible par tour
+    int nbStep;
+    /// 	
+    int level;
+    int experience;
+    bool turnPlayed;
+    int position;
+    bool movePlayed;
+    // Operations
+  public:
+    MobileElement ();
+    /// Renvoi "false"
+    bool const isStatic ();
+    virtual bool const isPlayerCharacter () = 0;
+    int const getNbStep ();
+    int const getNbAction ();
+    bool const getTurnPlayed ();
+    void setTurnPlayed (bool turnPlayed);
+    void setNbAction (int nbAction);
+    void setHP (int HP);
+    int const getHP ();
+    int const getAttackPt ();
+    void setAttackPt (int attackPt);
+    void const getDefensePt ();
+    void setDefensePt (int defensePt);
+    bool const getMovePlayed ();
+    void setMovePlayed (bool attackPlayed);
+  };
+
+  /// class StaticElement - 
+  class StaticElement : public state::Element {
+    // Operations
+  public:
+    /// Renvoi "true"
+    bool const isStatic ();
+    virtual bool const isSpace ();
+  };
+
+  /// class PlayerCharacter - 
+  class PlayerCharacter : public state::MobileElement {
+    // Attributes
+  protected:
+    /// Nombre de points de magie
+    int mana;
+    // Operations
+  public:
+    PlayerCharacter (TypeID id);
+    Element* const clone ();
+    Status const getStatus ();
+    bool const isPlayerCharacter ();
+  };
+
+  /// class Monster - 
+  class Monster : public state::MobileElement {
+    // Operations
+  public:
+    Monster (TypeID id);
+    Element* const clone ();
+    Status const getStatus ();
+    bool const isPlayerCharacter ();
+  };
+
+  enum ObstacleTypeID {
+    WALL,
+    STONE,
+    TREE,
+    FIR,
+    WATER
+  };
+
+  /// class Obstacle - 
+  class Obstacle : public state::StaticElement {
+    // Associations
+    state::ObstacleTypeID IDObstacle;
+    // Operations
+  public:
+    Obstacle (ObstacleTypeID id);
+    /// Renvoi false
+    bool const isSpace ();
+    Element* const clone ();
+    ObstacleTypeID const getObstableTypeID ();
+    void setObstacleTypeID (ObstacleTypeID id);
+  };
+
+  enum SpaceTypeID {
+    GRASS,
+    START,
+    ENERGY
+  };
+
+  /// class Space - 
+  class Space : public state::StaticElement {
+    // Associations
+    state::SpaceTypeID IDSpace;
+    // Operations
+  public:
+    Space (SpaceTypeID id);
+    /// Renvoi "true"
+    bool const isSpace ();
+    Element* const clone ();
+    SpaceTypeID const getSpaceTypeID ();
+    void setSpaceTypeID (SpaceTypeID id);
+  };
+
+  enum LevelStateEventID {
+    ALL_CHANGED,
+    LEVEL_CHANGED,
+    EPOCH_CHANGED,
+    LIST_CHANGED
+  };
+
+  /// class LevelStateEvent - 
+  class LevelStateEvent {
+    // Associations
+    state::LevelStateEventID id;
+    // Attributes
+  public:
+    const LevelState& levelState;
+    // Operations
+  public:
+    LevelStateEvent (const LevelState&  levelState, LevelStateEventID id);
+    bool const operator== (LevelStateEventID id);
+    bool const operator!= (LevelStateEventID id);
+  };
 
   /// class LevelStateObserver - 
   class LevelStateObserver {
     // Operations
   public:
-    virtual void levelStateChanged (const LevelStateEvent& ) = 0;
+    virtual void levelStateChanged (const LevelStateEvent& levelStateEvent) = 0;
+  };
+
+  /// class Observable - 
+  class Observable {
+    // Associations
+    // Attributes
+  protected:
+    std::vector<LevelStateObserver*> observers;
+    // Operations
+  public:
+    virtual ~Observable ();
+    void registerObserver (LevelStateObserver* o);
+    void notifyObservers (const LevelStateEvent& e e);
+  };
+
+  /// class ElementFactory - 
+  class ElementFactory {
+    // Attributes
+  protected:
+    std::map<char,AElementAlloc*> list;
+    // Operations
+  public:
+    ~ElementFactory ();
+    Element* const newInstance (char id);
+    void registerType (char id, AElementAlloc* a);
+  };
+
+  /// class ElementList - Contient le nombre d'éléments instanciés
+  class ElementList : public state::Observable {
+    // Associations
+    // Attributes
+  protected:
+    LevelState& levelState;
+    ElementFactory* factory;
+    std::vector<Element*> elements;
+    // Operations
+  public:
+    ElementList (LevelState& levelState);
+    ~ElementList ();
+    ElementList* const clone ();
+    void copy (const ElementList& list);
+    const LevelState& const getLevelState ();
+    int const size ();
+    Element* const getElement (int i);
+    state::Element* getElement (int x, int y);
+    void clear ();
+    void setElementFactory (ElementFactory* factory);
+    void setElement (int i, Element* element);
+    void removeElement (int i);
+    void removeElement (Element* element);
+    int numberOfPlayer ();
+    int numberOfMonster ();
+  };
+
+  /// class ElementGrid - Gère l'emplacement des éléments sur la grille
+  class ElementGrid : public state::ElementList {
+    // Attributes
+  protected:
+    int width;
+    int height;
+    // Operations
+  public:
+    ElementGrid (LevelState& levelState);
+    ElementGrid* const clone ();
+    void copy (const ElementGrid& grid);
+    int const getWidth ();
+    int const getHeight ();
+    /// Renvoi l'élément de la cellule selectionnee
+    /// @param i		(???) 
+    /// @param j		(???) 
+    Element* const getCell (int i, int j);
+    /// Place l'element sur la cellule selectionnee
+    /// @param i		(???) 
+    /// @param j		(???) 
+    /// @param e		(???) 
+    void setCell (int i, int j, Element* e);
+    void load (const char* file_name);
+  };
+
+  enum TurnToPlay {
+    PLAYER     = 0,
+    OPPONENT     = 1
+  };
+
+  enum WorldTypeID {
+    LEVEL     = 1
+  };
+
+  /// class ElementWorld - 
+  class ElementWorld {
+    // Associations
+    // Attributes
+  protected:
+    int x;
+    int y;
+     IDWorldElement;
+    bool active;
+    // Operations
+  public:
+    int const getX ();
+    int const getY ();
+    void setX (int x);
+    void setY (int y);
+  };
+
+  /// class ElementListWorld - 
+  class ElementListWorld {
+    // Associations
+    // Attributes
+  protected:
+    WorldState& worldState;
+    std::vector<ElementWorld*> worldElements;
+    // Operations
+  public:
+    ElementListWorld (WorldState& worldState);
+    WorldState& const getWorldState ();
+    int const size ();
+    ElementWorld* const getElement (int i);
+    void clear ();
+    void setElement (int i, ElementWorld* element);
+  };
+
+  /// class WorldState - 
+  class WorldState {
+    // Associations
+    // Attributes
+  protected:
+    WorldElementList worldElementList;
+    int epoch;
+    float epochRate;
+     nbLockedLevelLeft;
+    // Operations
+  public:
+    worldState ();
+    int const getEpoch ();
+    float const getEpochRate ();
+    void setEpoch (int time);
+    void setEpochRate (float rate);
+  };
+
+  enum StateType {
+    LEVELSTATE     = 1,
+    WORLDSTATE     = 2
+  };
+
+  /// class State - 
+  class State {
+    // Associations
+    // Attributes
+  protected:
+    LevelState levelState;
+    WorldState worldState;
+    StateType currentState;
+    // Operations
+  public:
+    ~State State ();
+  };
+
+  /// class LevelState - 
+  class LevelState : public state::Observable {
+    // Associations
+    // Attributes
+  protected:
+    ElementList characters;
+    ElementGrid grid;
+    int epoch;
+    float epochRate;
+    TurnToPlay turnToPlay;
+    /// Nombre de personnage restant dans l’équipe joueur 
+    int nbPlayerCharLeft;
+    /// Nombre de personnage restant dans l’équipe ennemi 
+    int nbOpponentCharLeft;
+    // Operations
+  public:
+    LevelState ();
+    LevelState* const clone ();
+    void copy (const LevelState& levelState);
+    int const getEpoch ();
+    float const getEpochRate ();
+    const ElementGrid& const getElementGrid ();
+    ElementGrid& getElementGrid ();
+    const ElementList& const getElementList ();
+    ElementList& getElementList ();
+    MobileElement* getMobileElem ();
+    TurnToPlay const getTurnToPlay ();
+    void setElementFactory (ElementFactory* factory);
+    void setEpoch (int epoch);
+    void setEpochrate (float rate);
+    void loadLevel (const char* file_name);
+  };
+
+  /// class AElementAlloc - 
+  class AElementAlloc {
+    // Associations
+    // Operations
+  public:
+    virtual ~AElementAlloc ();
+    virtual Element* newInstance ();
+  };
+
+  /// class ElementAlloc - 
+  template <  classe E,   typename ID>
+  class ElementAlloc : public state::AElementAlloc {
+    // Attributes
+  protected:
+    ID id;
+    // Operations
+  public:
+    ElementAlloc (ID id);
+    Element* newInstance ();
+  };
+
+  enum LevelType {
+    NORMAL     = 1,
+    BOSS     = 2
+  };
+
+  enum DifficultyLevel {
+    EASY     = 1,
+    MEDIUM     = 2,
+    HARD     = 3
+  };
+
+  /// class ElementLevel - 
+  class ElementLevel : public state::ElementWorld {
+    // Associations
+    // Attributes
+  protected:
+    LevelType levelType;
+    DifficultyLevel difficultyLevel;
+    bool locked;
+    // Operations
+  public:
+    bool const islocked ();
+    void unlock ();
+  };
+
+  /// class LevelListEvent - 
+  class LevelListEvent : public state::LevelStateEvent {
+    // Attributes
+  public:
+    const Element& list;
+    int idx;
+    // Operations
+  public:
+    LevelListEvent (const ElementList& list, int idx);
   };
 
 };
