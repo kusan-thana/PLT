9971b4b73607675195624b04fca1b1cf465c1b70
benoit lafon <benoit.lafon92@gmail.com>
2015-12-09 19:21:20 +0100

    state.hpp

---
+++ src/state.hpp
@@ -0,0 +1,36 @@
+#ifndef State_H
+#define State_H
+#include <vector>
+#include <unordered_map>
+
+namespace state {
+	class Element;
+	class MobileElement;
+	class StaticElement;
+	class PlayerCharacter;
+	class Monster;
+	class Obstacle;
+	class Space;
+	class ElementFactory;
+	class AElementAlloc;
+	//template<class E, typename ID>
+	class ElementAlloc;
+	class Observable;
+	class LevelStateObserver;
+	class LevelStateEvent;
+	class LevelListEvent;
+	class ElementList;
+	class ElementGrid;
+	class LevelState;
+	class State;
+	class WorldState;
+	class ElementListWorld;
+	class ElementWorld;
+	class ElementLevel;
+}
+
+#include "generated/state.h"
+#include "elementAlloc.tpp"		//Implementation file
+
+
+#endif
