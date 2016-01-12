5011dbda13fe6e28f7d0e1083395c8f04917bbce
kusan thanabalasingam <kusan.thana@gmail.com>
2016-01-12 10:04:34 +0100

    Request user OK

--- src/common/remoteServerSFML.cpp
+++ src/common/remoteServerSFML.cpp
@@ -1,5 +1,7 @@
 #include "server.hpp"
 #include <iostream>
+#include <SFML/Network.hpp>
+
 using namespace server;
 
 RemoteServerSFML::RemoteServerSFML(){
@@ -8,8 +10,23 @@ RemoteServerSFML::~RemoteServerSFML(){
 }
 void RemoteServerSFML::run(){
 
+    sf::Http::Request request("/user/1", sf::Http::Request::Get);
+    sf::Http http("http://localhost/", 8080);
+    sf::Http::Response response = http.sendRequest(request);
+	
+	std::cout << "REPONSE : " << response.getStatus();
+	if (response.getStatus() == sf::Http::Response::Ok)
+    {
+        // affichage de la réponse du serveur
+        std::cout << response.getBody() << std::endl;
+    }
+    else
+    {
+        std::cout << "request failed" << std::endl;
+    }
 	while(!quit)
 	{
+
 		if(levelState.getTurnToPlay() == state::PLAYER) {
 			if (iaAutoMode) {
 				//dumbAI.run(engine);
