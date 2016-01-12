9ced524222d90a8e1ee7db88fe4a6c9778481f6c
kusan thanabalasingam <kusan.thana@gmail.com>
2016-01-12 11:14:59 +0100

    horloge + add -A

---
+++ src/client/remoteServerSFML.cpp
@@ -0,0 +1,61 @@
+#include "../common/server.hpp"
+#include <iostream>
+#include <SFML/Network.hpp>
+#include <chrono>
+
+using namespace server;
+
+struct timer
+{
+    typedef std::chrono::steady_clock clock ;
+    typedef std::chrono::seconds seconds ;
+
+    void reset() { start = clock::now() ; }
+
+    unsigned long long seconds_elapsed() const
+    { return std::chrono::duration_cast<seconds>( clock::now() - start ).count() ; }
+
+    private: clock::time_point start = clock::now() ;
+};
+
+
+RemoteServerSFML::RemoteServerSFML(){
+}
+RemoteServerSFML::~RemoteServerSFML(){
+}
+void RemoteServerSFML::run(){
+	
+	timer t ;
+	
+
+	
+	while(!quit)
+	{
+		if (t.seconds_elapsed() > 1){		    
+			sf::Http::Request request("/user/1", sf::Http::Request::Get);
+			sf::Http http("http://localhost/", 8080);
+			sf::Http::Response response = http.sendRequest(request);
+			
+			std::cout << "REPONSE : " << response.getStatus();
+			if (response.getStatus() == sf::Http::Response::Ok)
+			{
+				// affichage de la réponse du serveur
+				std::cout << response.getBody() << std::endl;
+			}
+			else
+			{
+				std::cout << "request failed" << std::endl;
+			}
+			if(levelState.getTurnToPlay() == state::PLAYER) {
+				if (iaAutoMode) {
+					//dumbAI.run(engine);
+				}
+			}
+			else if (levelState.getTurnToPlay() == state::OPPONENT) {
+			}
+
+			engine.update();
+			t.reset();
+		}
+	}
+}
---
+++ src/server/localServer.cpp
@@ -0,0 +1,29 @@
+#include "../common/server.hpp"
+#include <iostream>
+#include <mutex>
+using namespace server;
+
+LocalServer::LocalServer(){
+
+	this->ais = new ai::HeuristicAI(engine.getLevelState());
+}
+LocalServer::~LocalServer(){
+}
+void LocalServer::run(){
+
+	while(!quit)
+	{
+		if(levelState.getTurnToPlay() == state::PLAYER) {
+			if (iaAutoMode) {
+				ais->run(engine);
+				//dumbAI.run(engine);
+			}
+		}
+		else if (levelState.getTurnToPlay() == state::OPPONENT) {
+			ais->run(engine);
+		}
+
+		engine.update();
+	}
+	
+}
