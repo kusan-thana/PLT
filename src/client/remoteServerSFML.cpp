afca982a5c3cb07d0540968ac4ebe3e49e706e99
Kusan <kusan.thana@gmail.com>
2016-01-14 17:56:51 +0100

    a

--- src/client/remoteServerSFML.cpp
+++ src/client/remoteServerSFML.cpp
@@ -29,28 +29,28 @@ void RemoteServerSFML::run(){
 	
 	while(!quit)
 	{
-		if (t.milliseconds_elapsed() > 200){		    
-			//~ sf::Http::Request request("/user/1", sf::Http::Request::Get);
-			//~ sf::Http http("http://localhost/", 8080);
-			//~ sf::Http::Response response = http.sendRequest(request);
+		if (t.milliseconds_elapsed() > 2000){		    
+			sf::Http::Request request("/command", sf::Http::Request::Get);
+			sf::Http http("http://localhost/", 8080);
+			sf::Http::Response response = http.sendRequest(request);
 			
-			//~ std::cout << "REPONSE : " << response.getStatus();
-			//~ if (response.getStatus() == sf::Http::Response::Ok)
-			//~ {
-				//~ // affichage de la réponse du serveur
-				//~ std::cout << response.getBody() << std::endl;
-			//~ }
-			//~ else
-			//~ {
-				//~ std::cout << "request failed" << std::endl;
-			//~ }
-			//~ if(levelState.getTurnToPlay() == state::PLAYER) {
-				//~ if (iaAutoMode) {
-					//~ //dumbAI.run(engine);
-				//~ }
-			//~ }
-			//~ else if (levelState.getTurnToPlay() == state::OPPONENT) {
-			//~ }
+			std::cout << "REPONSE : " << response.getStatus();
+			if (response.getStatus() == sf::Http::Response::Ok)
+			{
+				// affichage de la réponse du serveur
+				std::cout << response.getBody() << std::endl;
+			}
+			else
+			{
+				std::cout << "request failed" << std::endl;
+			}
+			if(levelState.getTurnToPlay() == state::PLAYER) {
+				if (iaAutoMode) {
+					//dumbAI.run(engine);
+				}
+			}
+			else if (levelState.getTurnToPlay() == state::OPPONENT) {
+			}
 			
 			engine.update();
 			t.reset();
@@ -72,7 +72,7 @@ void RemoteServerSFML::addCommand(engine::Command *cmd) {
 	else if(cmd->getTypeId() == 3){
 		jason["x"] = ((engine::MoveCommand*)cmd)->getPositionX();
 		jason["y"] = ((engine::MoveCommand*)cmd)->getPositionY();
-		jason["character"] = ((engine::MoveCommand*)cmd)->getCharacter();
+		jason["character"] = ((engine::MoveCommand*)cmd)->getCharacter();		
 	}
 	else if(cmd->getTypeId() == 4){
 		jason["attacker"] = ((engine::AttackCommand*)cmd)->getAttacker();
@@ -87,7 +87,7 @@ void RemoteServerSFML::addCommand(engine::Command *cmd) {
 	sf::Http::Response response = http.sendRequest(request);
 	
 	std::cout << "REPONSE : " << response.getStatus();
-	if (response.getStatus() == sf::Http::Response::Ok)
+	if (response.getStatus() == sf::Http::Response::NoContent)
 	{
 		// affichage de la réponse du serveur
 		std::cout << response.getBody() << std::endl;
